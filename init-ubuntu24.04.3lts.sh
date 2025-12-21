f047567b5810ad427bbd170a7a4d1c0b651127eb
Luke Howe <luke.howe212@gmail.com>
2025-12-21 14:44:02 +0800

    Fix formatting for Docker repository addition

--- init-ubuntu24.04.3lts.sh
+++ init-ubuntu24.04.3lts.sh
@@ -107,14 +107,14 @@ if [[ "$DOCKER_CHOICE" =~ ^[Yy]$ ]]; then
   sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
   sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-  # Add the repository to Apt sources:
-  sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-  Types: deb
-  URIs: https://download.docker.com/linux/ubuntu
-  Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-  Components: stable
-  Signed-By: /etc/apt/keyrings/docker.asc
-  EOF
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
   VERSION_STRING="5:28.5.2-1~ubuntu.24.04~noble"
   
